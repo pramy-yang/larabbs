6aa8e2e5c4fe6c4ff7fc5c6351ff12f7a36e5280
杨超 <ye599188297@163.com>
2019-02-18 18:23:19 +0800

    分类话题列表

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,9 @@ class Category extends Model
 {
     //
     protected $fillable = ['name', 'description',];
+
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
 }
