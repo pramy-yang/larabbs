9c3a7995e5fe7b1769526dc9104fb019379e2bd4
杨超 <ye599188297@163.com>
2019-02-19 15:58:24 +0800

    修复 XSS 注入漏洞

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -63,11 +63,11 @@
 
 @section('scripts')
     <script type="text/javascript" src="{{ asset('js/module.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('js/hotkeys.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('js/uploader.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('js/simditor.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('js/hotkeys.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('js/uploader.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('js/simditor.js') }}"></script>
 
-    <script>
+        <script>
         $(document).ready(function() {
             var editor = new Simditor({
                 textarea: $('#editor'),
