2e4b33005f3d39ce982360f42e961e7b352b118e
杨超 <ye599188297@163.com>
2019-02-21 11:46:00 +0800

    用户最后活跃时间

---
+++ app/Console/Commands/SyncUserActivedAt.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use App\Models\User;
+
+class SyncUserActivedAt extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'larabbs:sync-user-actived-at';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '同步用户上次活跃时间';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle(User $user)
+    {
+
+        $user->syncUserActivedAt();
+        $this->info("同步成功！");
+    }
+}
